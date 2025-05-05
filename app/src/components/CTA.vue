4b7b60c0451cd4ccd200ad544834572c713386a5
NellitG <kamaunelson157@gmail.com>
2025-05-05 18:41:57 +0300

    created CTA component

---
+++ app/src/components/CTA.vue
@@ -0,0 +1,33 @@
+<template>
+    <section id="relume" class="relative px-[5%] py-16 md:py-24 lg:py-28">
+  <div class="container relative z-10 max-w-lg justify-center">
+    <h2 class="rb-5 mb-5 text-5xl font-bold text-text-alternative md:mb-6 md:text-7xl lg:text-4xl">
+        Plan Your Next Adventure
+    </h2>
+    <p class="text-text-alternative md:text-md">
+        Discover our diverse travel plans and embark on an unforgettable journey with Wanderly today!
+    </p>
+    <div class="mt-6 flex flex-wrap items-center justify-center gap-4 md:mt-8">
+      <button
+        class="focus-visible:ring-border-primary inline-flex gap-3 items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-border-primary bg-background-alternative text-text-alternative px-6 py-3"
+        title="Button"
+      >
+        Explore</button
+      ><button
+        class="focus-visible:ring-border-primary inline-flex gap-3 items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-border-alternative text-text-alternative px-6 py-3"
+        title="Button"
+      >
+        Get Started
+      </button>
+    </div>
+  </div>
+  <div class="absolute inset-0 z-0">
+    <img
+      src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg"
+      class="size-full object-cover"
+      alt="Relume placeholder image"
+    />
+    <div class="absolute inset-0 bg-black/50"></div>
+  </div>
+</section>
+</template>
