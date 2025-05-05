d656c0f45a75acdfce917a06b4c334672ccb83c0
NellitG <kamaunelson157@gmail.com>
2025-05-05 18:42:58 +0300

    created journey component

---
+++ app/src/components/Journey.vue
@@ -0,0 +1,132 @@
+<template>
+    <section id="relume" class="px-[5%] py-16 md:py-24 lg:py-28 bg-orange-100">
+  <div class="container">
+    <div
+      class="mb-12 grid grid-cols-1 items-start gap-5 md:mb-18 md:grid-cols-2 md:gap-x-12 lg:mb-20 lg:gap-x-20"
+    >
+      <h2 class="text-4xl font-bold leading-[1.2] md:text-5xl lg:text-5xl">
+        Your Journey Begins: Discover How to Book with Tusafiri
+      </h2>
+      <p class="md:text-md">
+        Tusafiri's travel plans offer unmatched convenience and cost savings. Experience personalized adventures tailored to your preferences.
+      </p>
+    </div>
+    <div class="grid grid-cols-1 items-start gap-y-12 md:grid-cols-3 md:gap-x-8 lg:gap-x-12">
+      <div>
+        <div class="mb-5 md:mb-6">
+          <img
+            src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+            class="size-12"
+            alt="Relume logo 1"
+          />
+        </div>
+        <h3 class="mb-3 text-xl font-bold md:mb-4 md:text-2xl">
+            Step 1: Choose Your Destination and Travel Dates
+        </h3>
+        <p>
+            Select where and when you want to travel.
+        </p>
+        <div class="mt-6 flex items-center gap-4 md:mt-8">
+          <button
+            class="focus-visible:ring-border-primary inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-0 text-text-primary gap-2 p-0"
+            title="Button"
+          >
+            Next<svg
+              stroke="currentColor"
+              fill="none"
+              stroke-width="0"
+              viewBox="0 0 15 15"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div>
+        <div class="mb-5 md:mb-6">
+          <img
+            src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+            class="size-12"
+            alt="Relume logo 2"
+          />
+        </div>
+        <h3 class="mb-3 text-xl font-bold md:mb-4 md:text-2xl">
+            Step 2: Customize Your Travel Experience
+        </h3>
+        <p>
+            Tailor your itinerary with activities that excite you.
+        </p>
+        <div class="mt-6 flex items-center gap-4 md:mt-8">
+          <button
+            class="focus-visible:ring-border-primary inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-0 text-text-primary gap-2 p-0"
+            title="Button"
+          >
+            Continue<svg
+              stroke="currentColor"
+              fill="none"
+              stroke-width="0"
+              viewBox="0 0 15 15"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div>
+        <div class="mb-5 md:mb-6">
+          <img
+            src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+            class="size-12"
+            alt="Relume logo 3"
+          />
+        </div>
+        <h3 class="mb-3 text-xl font-bold md:mb-4 md:text-2xl">
+            Step 3: Review and Confirm Your Travel Plan
+        </h3>
+        <p>
+            Double-check your selections and finalize your booking.
+        </p>
+        <div class="mt-6 flex items-center gap-4 md:mt-8">
+          <button
+            class="focus-visible:ring-border-primary inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-0 text-text-primary gap-2 p-0"
+            title="Button"
+          >
+            Book<svg
+              stroke="currentColor"
+              fill="none"
+              stroke-width="0"
+              viewBox="0 0 15 15"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
