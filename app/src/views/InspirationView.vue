f23cddbdf25500486765f65dd03d4aaaea87f558
NellitG <kamaunelson157@gmail.com>
2025-05-09 18:39:53 +0300

    added CustomerTestimonial component in inspiration view

--- app/src/views/InspirationView.vue
+++ app/src/views/InspirationView.vue
@@ -1,11 +1,13 @@
 <template>
    <Insights />
-   <Inspire />
-   
+   <!-- <Inspire /> -->
+   <CustomerTestimonial />
+
     </template>
     
     <script setup>
     import Inspire from '@/components/Inspire.vue'
     import Insights from '../components/Insights.vue';
+    import CustomerTestimonial from '../components/CustomerTestimonial.vue';
     </script>
     
