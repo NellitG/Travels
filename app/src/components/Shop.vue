0ca8cc0912613207d350e6fd0197db642d55e8b7
NellitG <kamaunelson157@gmail.com>
2025-06-01 17:39:09 +0300

    changed names of product

--- app/src/components/Shop.vue
+++ app/src/components/Shop.vue
@@ -228,7 +228,7 @@
         </div>
         <div class="p-4">
           <div class="flex justify-between items-start">
-            <h3 class="font-semibold">Smartphone</h3>
+            <h3 class="font-semibold">Bracelets</h3>
             <button class="text-gray-400 hover:text-red-500">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
@@ -260,7 +260,7 @@
         </div>
         <div class="p-4">
           <div class="flex justify-between items-start">
-            <h3 class="font-semibold">Laptop</h3>
+            <h3 class="font-semibold">Camera</h3>
             <button class="text-gray-400 hover:text-red-500">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
@@ -292,7 +292,7 @@
         </div>
         <div class="p-4">
           <div class="flex justify-between items-start">
-            <h3 class="font-semibold">Smartphone</h3>
+            <h3 class="font-semibold">Shades of gray</h3>
             <button class="text-gray-400 hover:text-red-500">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
