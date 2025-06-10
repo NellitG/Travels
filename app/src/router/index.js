ea729474cd764532aee1795b8e3504586e6fd807
NellitG <kamaunelson157@gmail.com>
2025-06-10 10:54:10 +0300

    add path router on components

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { HomeView, PostsView, DetailsView, LoginView, TravelPlanView, InspirationView, ShopView } from "../views";
+import { HomeView, PostsView, DetailsView, LoginView, TravelPlanView, InspirationView, ShopView, SignUpView, DestinationView } from "../views";
 import { useAuthStore } from "../store";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  // { path: "/login", name: "login", component: LoginView },
+  { path: "/login", name: "login", component: LoginView },
   { path: "/posts", name: "posts", component: PostsView },
   { path: "/posts/:id", name: "details", component: DetailsView, props: true },
   {path:"/travelplan", name:"Travelplan", component: () => import("../views/TravelPlanView.vue")},
   {path:"/inspiration", name:"Inspiration", component: () => import("../views/InspirationView.vue")},
-  {path:"/shop", name:"Shop", component: () => import("../views/ShopView.vue")}
+  {path:"/shop", name:"Shop", component: () => import("../views/ShopView.vue")},
+  {path: "/SignUp", name: "signup", component: () => import("../views/SignUpView.vue")},
+  {path: "/destination", name: "destination", component: () => import("../views/DestinationView.vue")},
 ];
 
 /**Initialize here */
